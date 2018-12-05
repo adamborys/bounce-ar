d0531feb9cd35eafbff7662581944197e0affbe4
Adam Borys <hipowolemia@gmail.com>
2018-12-06 00:00:39 +0100

    Game Scene WIP

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GameController : MonoBehaviour 
+{
+	GameObject userTarget;
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy(GameObject.Find("TestCube"));
+		userTarget = GameObject.Find("UserTarget");
+		transform.SetParent(userTarget.transform, false);
+	}
+}
